87afa37ab16af558e98afc044cd3363302bf58a6
hustcoin <yuanmi@hust.edu.cn>
2016-12-19 14:46:59 +0800

    Initial

---
+++ BigInteger.cpp
@@ -0,0 +1,229 @@
+#include "BigInteger.h"
+#include <algorithm>
+#include <iterator>
+#include <cctype>
+using std::swap;
+using std::cerr;
+
+//------------------------------ Constructor
+BigInteger::BigInteger(const string &str): number(str), sign(true) {
+	number.erase(number.begin(),
+				 find_if_not(number.begin(), number.end(), [](char c){return std::isspace(c);})
+				 ); //remove the leading spaces
+	number.erase(find_if_not(number.rbegin(), number.rend(), [](char c){return std::isspace(c);}).base(),
+				 number.end()
+				 ); //remove the trailing spaces
+	if(number.empty()) {
+		cerr << "Error: BigInteger is empty!\n";
+		number = "0"; //set BigInteger to  0
+	}
+	else {
+		if (number[0] == '-'){
+			number.erase(number.begin());
+			sign = false;
+		}
+		else {
+			if(number[0] == '+'){
+				number.erase(number.begin());
+			}
+		}
+		if (number.empty()){
+			cerr << "Error: BigInteger only have sign\n";
+			number = "0"; //set BigInteger to  0
+		}
+		else {
+			if (number[0] == '0' && number.size() > 1) {
+				cerr << "Error: unexpected leading zero!\n";
+				number = "0"; //set BigInteger to 0;
+			}
+			else{
+				auto first = find_if_not(number.begin(), number.end(), [](char c) {return std::isdigit(c);});
+				if (first != number.end()){
+					cerr << "Error: BigInteger must be digits\n";
+					number = "0"; //set BigInteger to  0
+				}
+				else{
+					if (number[0] == '0') sign = true;
+				}
+			}
+		}
+	}
+}
+//------------------------------ operator<< 
+ostream &operator<<(ostream &os, const BigInteger &bi){
+	if (bi.sign) 
+		os << bi.number;
+	else
+		os << "-" << bi.number;
+	return os;
+}
+//------------------------------ operator>>
+istream &operator>>(istream &is, BigInteger &bi){
+	is >> bi.number; 
+	if(!is)
+		bi = BigInteger();
+	else
+		bi = BigInteger(bi.number);
+	return is;
+}
+//------------------------------ operator== 
+bool operator==(const BigInteger &lhs, const BigInteger &rhs) {
+	return lhs.number == rhs.number && lhs.sign == rhs.sign;
+}
+	
+//------------------------------  operator!=
+bool operator!=(const BigInteger &lhs, const BigInteger &rhs) {
+	return !(lhs == rhs);
+}
+
+//------------------------------  operator >
+bool operator>(const BigInteger &lhs, const BigInteger &rhs) {
+	if (lhs.sign > rhs.sign)
+		return true;
+	else{
+		if (lhs.sign < rhs.sign)
+			return false;
+		else
+			if (lhs.sign){
+				if (lhs.number.size() > rhs.number.size())
+					return true;
+				else {
+					if (lhs.number.size() < rhs.number.size())
+						return false;
+					else {
+						return lhs.number > rhs.number;
+					}
+				}
+			}
+			else
+				return !(~lhs > ~rhs);
+	}
+}
+//------------------------------  operator >=
+bool operator>=(const BigInteger &lhs, const BigInteger &rhs) {
+	return lhs == rhs || lhs > rhs;
+}
+//------------------------------  operator <
+bool operator<(const BigInteger &lhs, const BigInteger &rhs) {
+	return !(lhs >= rhs);
+}
+//------------------------------  operator <=
+bool operator<=(const BigInteger &lhs, const BigInteger &rhs) {
+	return !(lhs > rhs);
+}
+//------------------------------  operator ~
+BigInteger BigInteger::operator~() const {
+	if (number[0] == '0')
+		return *this;
+	else{
+		BigInteger temp = *this;
+		temp.sign = !sign;
+		return temp;
+	}
+}
+//------------------------------  operator !
+bool BigInteger::operator!() const {
+	if (number[0] != '0')
+		return false;
+	else
+		return true;
+}
+//------------------------------  operator +
+BigInteger operator+(const BigInteger &lhs, const BigInteger &rhs){
+	if (lhs.sign != rhs.sign)
+		return lhs - ~rhs;
+	else {
+		if (lhs.number.size() >= rhs.number.size()){
+			BigInteger sum = lhs;
+			for (string::size_type i = 0; i != rhs.number.size(); ++i)
+				sum.number[sum.number.size()-i-1] +=
+						rhs.number[rhs.number.size()-i-1] - '0';
+			int carry = 0, temp = 0;
+			for (string::size_type i = 0; i != sum.number.size(); ++i){
+				temp = sum.number[sum.number.size()-i-1] - '0' + carry;  
+				sum.number[sum.number.size()-i-1] = temp % 10 + '0';
+				carry = temp / 10;
+			}
+			if (carry == 1)
+				sum.number.insert(sum.number.begin(), '1');
+			return sum;
+		}
+		else 
+			return rhs + lhs;
+	}
+}
+//------------------------------  operator +=
+BigInteger& BigInteger::operator+=(const BigInteger &rhs){
+	*this = *this + rhs;
+	return *this;
+}
+//------------------------------  operator -
+BigInteger operator-(const BigInteger &lhs, const BigInteger &rhs){
+	if (lhs.sign != rhs.sign)
+		return lhs + ~rhs;
+	else {
+		if (lhs.sign) {
+			if (lhs >= rhs) {
+				BigInteger diff = lhs;
+				for (string::size_type i = 0; i != rhs.number.size(); ++i)
+					diff.number[diff.number.size()-i-1] -=
+						(rhs.number[rhs.number.size()-i-1] - '0');
+				int carry = 0, temp =0;
+				for (string::size_type i = 0; i != diff.number.size(); ++i){
+					temp = diff.number[diff.number.size()-i-1] - '0' - carry;
+					diff.number[diff.number.size()-i-1] = (temp + 10) % 10 + '0';
+					carry = temp < 0 ? 1 : 0;
+				}
+				auto first = find_if(diff.number.begin(), diff.number.end(),
+									[](char c) {return c != '0';}
+									);
+				if (first == diff.number.end())
+					diff.number = "0";
+				else
+					diff.number.erase(diff.number.begin(), first);
+				return diff;
+			}
+			else
+				return ~(rhs - lhs);
+		}
+		else
+			return ~(~lhs - ~rhs);
+	}
+}
+//------------------------------  operator -=
+BigInteger& BigInteger::operator-=(const BigInteger &rhs){
+	*this = *this - rhs;
+	return *this;
+}
+//------------------------------  operator *
+BigInteger operator*(const BigInteger &lhs, const BigInteger &rhs){
+	if (lhs.number.size() >= rhs.number.size()) {
+		BigInteger prod;
+		prod.sign = !(lhs.sign ^ rhs.sign);
+		for (string::size_type i = 0; i != rhs.number.size(); ++i) {
+			if (rhs.number[rhs.number.size()-i-1] == '0')
+				continue;
+			BigInteger prod_temp = lhs;
+			int carry = 0, temp = 0;
+			for (string::size_type k =0; k != lhs.number.size(); ++k) {
+				temp =  (lhs.number[lhs.number.size()-k-1] - '0') *
+						(rhs.number[rhs.number.size()-i-1] - '0') + carry;
+				prod_temp.number[prod_temp.number.size()-k-1] = temp % 10 + '0';
+				carry = temp / 10;
+			}
+			if (carry > 0)
+				prod_temp.number.insert(prod_temp.number.begin(), carry + '0');
+			for (string::size_type j = 0; j != i; ++j)
+				prod_temp.number.push_back('0');
+			prod += prod_temp;
+		}
+		return prod;
+	}
+	else
+		return rhs * lhs;
+}
+//------------------------------  operator **
+BigInteger& BigInteger::operator*=(const BigInteger &rhs){
+	*this = *this * rhs;
+	return *this;
+}
---
+++ BigInteger.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::to_string;
+using std::ostream;
+using std::istream;
+
+class BigInteger{
+	friend ostream& operator<<(ostream&, const BigInteger&);
+	friend istream& operator>>(istream&, BigInteger&); 
+	friend bool operator==(const BigInteger&, const BigInteger&);
+	friend bool operator!=(const BigInteger&, const BigInteger&);
+	friend bool operator>(const BigInteger&, const BigInteger&);
+	friend bool operator>=(const BigInteger&, const BigInteger&);
+	friend bool operator<(const BigInteger&, const BigInteger&);
+	friend bool operator<=(const BigInteger&, const BigInteger&);
+	friend BigInteger operator+(const BigInteger&, const BigInteger&);
+	friend BigInteger operator-(const BigInteger&, const BigInteger&);
+	friend BigInteger operator*(const BigInteger&, const BigInteger&);
+public:
+	BigInteger(const string &str = string("0")); 
+	BigInteger(const char *c): BigInteger(string(c)) { }
+	BigInteger(const long long &l):
+			BigInteger(to_string(l)) { }
+	BigInteger(const int &i):
+			BigInteger(to_string(i)) { }
+	BigInteger operator~() const;
+	bool operator!() const;
+	BigInteger& operator+=(const BigInteger&);
+	BigInteger& operator-=(const BigInteger&);
+	BigInteger& operator*=(const BigInteger&);
+	BigInteger& abs() {sign = true; return *this;};
+private:
+	string number;
+	bool sign;
+};
